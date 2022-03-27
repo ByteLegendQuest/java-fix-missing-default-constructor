2015cf4d26256550bc948e8f8ab03b0d4247d5ff
daydreaming666 <indigodream@foxmail.com>
2022-03-27 10:09:54 +0000

    Update at 2022-03-27T10:09:54.818383Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -4,7 +4,11 @@ public class Cat {
     public String name;
 
     public Cat(String name) {
-        name = name;
+        this.name = name;
+    }
+
+    public Cat() {
+        this("");
     }
 
     @Override
