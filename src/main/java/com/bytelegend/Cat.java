3dc93628c7c3adc8205d0f71463755f732ce1783
lkskjjk <yun.11@yandex.com>
2022-05-05 08:29:14 +0000

    Update at 2022-05-05T08:29:14.347898Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,9 +2,10 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
+    public Cat() { }
 
     public Cat(String name) {
-        name = name;
+        this.name = name;
     }
 
     @Override
