0068e44ad4b1cb3ee615287d782a30f05859e81f
zhyz0727 <zhyz0727@163.com>
2022-05-16 04:58:24 +0000

    Update at 2022-05-16T04:58:24.591658Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -7,6 +7,10 @@ public class Cat {
         name = name;
     }
 
+    public Cat() {
+        this("");
+    }
+    
     @Override
     public String toString() {
         return "Cat(" + name + ")";
