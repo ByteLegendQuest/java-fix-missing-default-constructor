76bf95281f301ea7e31c64b32edaa775adfad619
Birlinha <omenaribeiro13@gmail.com>
2022-07-12 03:10:54 +0000

    Update at 2022-07-12T03:10:54.866194Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,9 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
-    public Cat() {}
-
     public Cat(String name) {
+        this.name = name;
+    }
+
+    public Cat() {
         name = name;
     }
 
