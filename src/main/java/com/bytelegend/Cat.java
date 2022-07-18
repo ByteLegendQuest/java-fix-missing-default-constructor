fae7350f54ac181898363cce2fe7e814eac30b2e
amine01laaboudi <al2000229@gmail.com>
2022-07-18 02:37:34 +0000

    Update at 2022-07-18T02:37:34.307697Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,9 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    Cat() {
+        this("");
+    }
     public Cat(String name) {
         this.name = new String(name);
     }
