cb2cf2380d6f9652e0923a6e598adb42fdbf5e8d
slsy0 <1485807135@qq.com>
2022-08-29 09:02:31 +0000

    Update at 2022-08-29T09:02:31.345905Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -4,9 +4,11 @@ public class Cat {
     public String name;
 
     public Cat(String name) {
-        this.name = name;
+        name = name;
     }
-
+    public Cat() {
+        
+    }
     @Override
     public String toString() {
         return "Cat(" + name + ")";
