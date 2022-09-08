4e7fc62a4b3c506969b1378ddce7ece04c500e4e
gakki5201314 <1246278573@qq.com>
2022-09-08 05:54:48 +0000

    Update at 2022-09-08T05:54:48.779532Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,7 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
-    public Cat(String name) {
+    public Cat() {
+
+    }
+
+    public Cat(String name){
         this.name = name;
     }
 
