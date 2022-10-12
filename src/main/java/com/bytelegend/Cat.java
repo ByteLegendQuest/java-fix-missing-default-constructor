1c8e49c33879ba1071ebaf01e8f76827aba3ac53
tiger0961 <29515547@qq.com>
2022-10-12 02:31:57 +0000

    Update at 2022-10-12T02:31:57.940321Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,9 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(){}
+
     public Cat(String name) {
         name = name;
-    }
+    } 
 
     @Override
     public String toString() {
