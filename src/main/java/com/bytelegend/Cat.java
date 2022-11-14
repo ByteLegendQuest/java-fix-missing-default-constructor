f9cbe5ae1ded185bcd1e68a6e5995e1d093c85f7
Newelle <wozi1988@proton.me>
2022-11-14 03:58:12 +0000

    Update at 2022-11-14T03:58:12.777510Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,7 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
-    public Cat(String name) {
+    Cat(){
+        
+    }
+
+    Cat(String name) {
         this.name = name;
     }
 
