40d19ef64095a792226bbd4285db4c216a757eaf
johnsonxiaoyu <1272304153@qq.com>
2022-11-29 07:21:12 +0000

    Update at 2022-11-29T07:21:12.431615Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,10 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat() {
+        name = name;
+    }
+    
     public Cat(String name) {
         this.name = name;
     }
