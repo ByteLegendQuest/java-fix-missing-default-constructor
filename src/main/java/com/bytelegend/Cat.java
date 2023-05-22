72be8c820012149914e838a305b4e7849c1e316e
NahuelBullonJensen <nahuelbullonjensen@gmail.com>
2023-05-22 00:35:01 +0000

    Update at 2023-05-22T00:35:01.621632Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,8 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat() {
+        name = "";
+    }
     public Cat(String name) {
-        name = name;
+        this.name = name;
     }
 
     @Override
